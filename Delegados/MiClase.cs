de45c568d108fc7daf7753b9873f9085a19c2cb4
cedoduarte <carlosduarte.1@hotmail.com>
2015-11-29 19:46:38 -0600

    first commit

---
+++ Delegados/MiClase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegados
+{
+    public class MiClase
+    {
+        public void Inicia()
+        {
+            Ejecuta(Proceso1);
+            Ejecuta(Proceso2);
+            Proceso1();
+            Proceso2();
+            Console.Read();
+        }
+
+        private void Ejecuta(Proceso proceso)
+        {
+            proceso();
+        }
+
+        private void Proceso1()
+        {
+            Console.WriteLine("Proceso 1");
+        }
+
+        private void Proceso2()
+        {
+            Console.WriteLine("Proceso 2");
+        }
+
+        private delegate void Proceso();
+    }
+}
